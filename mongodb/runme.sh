a29abd7a6afab3010b30aff68b9ff48b8ca52792
darthguinea <darthguinea@protonmail.com>
2018-04-09 14:02:55 +1000

    Improved runme

--- mongodb/runme.sh
+++ mongodb/runme.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+LOCATION=$(pwd)
+
 docker system prune -f
-docker stop mongodb
+
+RUNNING=$(docker ps -a | grep 'mongodb$' | awk '{print $1}')
+if [ ${RUNNING} != "" ]
+then
+    docker stop ${RUNNING}
+fi
 
 docker build -t mongodb .
-docker run -v files/data:/data --rm -dt -p 27017:27017 --name mongodb mongodb
+docker run --rm -dt -p 27017:27017 -v ${LOCATION}/files/data/:/data --name mongodb mongodb
